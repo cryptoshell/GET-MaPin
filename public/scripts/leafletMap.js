4aa6233b9d4a6ca2e4c41d8b04fb45aa0bdfe3a7
Yelim Oh <yelim.oh1@gmail.com>
2017-07-20 04:22:46 +0000

    Add leaflet marker function to add and edit

--- public/scripts/leafletMap.js
+++ public/scripts/leafletMap.js
@@ -1,5 +1,6 @@
 $().ready(function(accessKey){
   // this function sets up the base map tile layer and hardcodes the public access token.
+
   let map = L.map('mapid').setView([49.2827, -123.1088], 15);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -7,14 +8,71 @@ $().ready(function(accessKey){
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoia2FpMTkiLCJhIjoiY2o1Yml4a204MDA5ODJxbXIyM3NvYXh1NCJ9.PJ53vssI0g2s8F-8-nA6qQ'
   }).addTo(map);
-  // this adds a marker to specfic lat-long and puts an example popup.
-  L.marker([49.2830, -123.1088]).addTo(map)
-  .bindPopup('My favourite Cafe!<br>Pinned by Yelim')
-  .openPopup();
-  L.marker([49.2842, -123.1088]).addTo(map)
-  .bindPopup('Best place to study!<br>Pinned by Nikolai')
-  .openPopup();
-  L.marker([49.2820, -123.1088]).addTo(map)
-  .bindPopup('Cat Cafe!<br>Pinned by Kai')
-  .openPopup();
+
+  // // initialize the map on the "map" div with a given center and zoom
+  // let map = L.map('mapid').setView([49.2827, -123.1088], 15).addlayer(osm);
+
+  // map.on('click', onMapClick);
+
+  // // Script for adding marker on map click
+  // function onMapClick(e) {
+  //   let geojsonFeature = {
+  //       "type": "Feature",
+  //           "properties": {},
+  //           "geometry": {
+  //               "type": "Point",
+  //               "coordinates": [e.latlng.lat, e.latlng.lng]
+  //       }
+  //   }
+
+  //   let marker;
+
+  //   L.geoJson(geojsonFeature, {
+
+  //       pointToLayer: function(feature, latlng){
+
+  //           marker = L.marker(e.latlng, {
+
+  //               title: "Resource Location",
+  //               alt: "Resource Location",
+  //               riseOnHover: true,
+  //               draggable: true,
+
+  //           }).bindPopup("<input type='button' value='Delete this marker' class='marker-delete-button'/>");
+
+  //           marker.on("popupopen", onPopupOpen);
+
+  //           return marker;
+  //       }
+  //   }).addTo(map);
+  // }
+
+  // // Function to handle delete as well as other events on marker popup open
+  // function onPopupOpen() {
+  //   let tempMarker = this;
+
+  //   // To remove marker on click of delete
+  //   $(".marker-delete-button:visible").click(function () {
+  //       map.removeLayer(tempMarker);
+  //   });
+  // }
+
+  // // getting all the markers at once
+  // function getAllMarkers() {
+  //   let allMarkersObjArray = [];//new Array();
+  //   let allMarkersGeoJsonArray = [];//new Array();
+
+  //   $.each(map._layers, function (ml) {
+  //     //console.log(map._layers)
+  //     if (map._layers[ml].feature) {
+  //       allMarkersObjArray.push(this)
+  //       allMarkersGeoJsonArray.push(JSON.stringify(this.toGeoJSON()))
+  //     }
+  //   })
+
+  //   console.log(allMarkersObjArray);
+  //   alert("total Markers : " + allMarkersGeoJsonArray.length + "\n\n" + allMarkersGeoJsonArray + "\n\n Also see your console for object view of this array" );
+  // }
+
+  // $(".get-markers").on("click", getAllMarkers);
 });
