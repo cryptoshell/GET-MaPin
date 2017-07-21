9d43c747bbf726126a01b583caccc6425849eff2
Yelim Oh <yelim.oh1@gmail.com>
2017-07-21 16:31:41 +0000

    Update server.js with other npm packages

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,47 @@
 "use strict";
 
-const express = require('express');
+const express = require("express");
 const router  = express.Router();
 
-router.get("/register", function (req, res) {
-    res.render("register")
-});
+module.exports = (knex) => {
 
-module.exports = router;
+  router.get("/register", (req, res) => {
+      res.render("register")
+  });
+
+  router.post("/register", (req, res) => {
+    if (!req.body.email || !req.body.password) {
+      res.send('blank email/pw!');
+      req.flash("errors", "email and password cannot be blank!");
+      res.redirect("/");
+      return;
+    }
+
+    const matchProvidedEmail = knex("users")
+      .select(1)
+      .where({email: req.body.email})
+      .limit(1);
+    matchProvidedEmail.then((rows) => {
+      if (rows.length) {
+        return Promise.reject({
+          type: 409,
+          message: "email already exists"
+        });
+      }
+      return bcrypt.hash(req.body.password, 10);
+    }).then((passwordDigest) => {
+      return knex("users").insert({
+        name: req.body.name,
+        email: req.body.email,
+        password: passwordDigest
+      });
+    }).then(() => {
+      req.flash("info", "account created successfully");
+      res.redirect("/");
+    }).catch((err) => {
+      req.flash('errors', err.message);
+      res.redirect("/");
+    });
+  });
+  return router;
+}
