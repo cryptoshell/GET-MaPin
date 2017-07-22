6c0b0cceb456d6b007c3fa8862f078c926ebabb1
Yelim Oh <yelim.oh1@gmail.com>
2017-07-22 04:42:12 +0000

    Query marker data from db

--- routes/marker.js
+++ routes/marker.js
@@ -5,23 +5,37 @@ const express = require("express");
 function createRouter(knex) {
   const router  = express.Router();
 
-  router.post("/", (req, res) => {
+  // router.get("/", (req, res) => {
 
-    const addNewMarker = knex("markers").insert({
-          users_id: req.session.user_id,
-          categories_id: 1,
-          lat: req.body.lat,
-          long: req.body.long,
-          title: req.body.title,
-          description: req.body.description
-        });
+    // knex('markers').select().where('categories_id', 1).then((resultArray) => {
+    //     let listsOfMarkers = {};
+    //     resultArray.forEach((row) =>{
+    //       listsOfMarkers[row.id] = row.name;
+    //     })
+    //     const templateVars = {
+    //       listsMarker: listsOfMarkers,
+    //       user: req.session.user_id
+    //     }
+    //     res.render("index", templateVars);
+    //   })
 
-    addNewMarker.then((marker) => {
-        res.json(marker);
-      }).catch((error) => {
-        res.sendStatus(500);
-    });
-  });
+  // router.post("/", (req, res) => {
+
+  //   const addNewMarker = knex("markers").insert({
+  //         users_id: req.session.user_id,
+  //         categories_id: 1,
+  //         lat: req.body.lat,
+  //         long: req.body.long,
+  //         title: req.body.title,
+  //         description: req.body.description
+  //       });
+
+  //   addNewMarker.then((marker) => {
+  //       res.json(marker);
+  //     }).catch((error) => {
+  //       res.sendStatus(500);
+  //   });
+  // });
   return router;
 }
 
