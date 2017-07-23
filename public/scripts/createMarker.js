410738e1aa996b2dedd70b26c26c152857658e0e
Yelim Oh <yelim.oh1@gmail.com>
2017-07-23 01:12:08 +0000

    Make render and create marker functions and link it in index.ejs

--- public/scripts/createMarker.js
+++ public/scripts/createMarker.js
@@ -1,6 +1,6 @@
 $(() => {
 
-// Initializes map and centers to lat-long
+  // Initializes map and centers to lat-long
   const initMap = () => {
     const location = { lat: 49.2827, lng: -123.1088 };
     const map = new google.maps.Map(document.getElementById('map'), {
@@ -44,43 +44,6 @@ $(() => {
       popUp.open(map, marker);
     });
   }
-
-
-
-
-
-
-
-
-
-    // // Set pop-up content
-    // for (let marker in markers) {
-    //   var popUpContent = `
-    //   <div id="${marker.id}">
-    //       <h3>${marker.title}</h3>
-    //       <p>${marker.description}</p>
-    //       <p>category: ${marker.categories_id}</p>
-    //   </div>`;
-
-    //   // Initialize new pop-up
-    //   const popUp = new google.maps.InfoWindow({
-    //     content: popUpContent
-    //   });
-
-    //   // Create a new marker
-    //   const pin = new google.maps.Marker({
-    //     position: { lat: marker.lat, lng: marker.long },
-    //     map: map,
-    //     title: marker.title
-    //   });
-
-    //   marker.addListener('click', function() {
-    //     popUp.open(map, pin);
-    //   });
-    // }
-
-
-
   initMap();
 
 });
