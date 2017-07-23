464513d8006e023d31000b9c89f49b5ba89dfb4f
Nikolai <nikolaigauer@gmail.com>
2017-07-23 04:16:45 +0000

    Add funcitonality so to show username when logged in

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,15 @@ function createRouter(knex) {
   const router  = express.Router();
 
   router.get("/users", (req, res) => {
-    let templateVars = {user: req.session.user_id};
+    knex('users').select('*').where('id', req.session.user_id).limit(1)
+    .then((result)=> {
+    let templateVars = {
+      user: req.session.user_id,
+      name: result[0].name};
     res.render("users", templateVars);
+    }
+  )
+
   });
   return router;
 }
