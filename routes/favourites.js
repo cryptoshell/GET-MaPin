a3488f7989fd0fce8e33529bc4d59b0ada5d072a
Kai19 <ngyankai@hotmail.com>
2017-07-23 22:56:00 +0000

    prevented unauthenticated from submitted favourites

--- routes/favourites.js
+++ routes/favourites.js
@@ -4,16 +4,20 @@ const express = require("express");
 
 function createRouter(knex) {
   const router  = express.Router();
-  
+
   router.post("/", (req, res) => {
-    knex("favourites").insert({
-      users_id: req.session.user_id,
-      categories_id: req.body.favourite
-    }).then((favourites) => {
+    if(req.session.user_id){
+      knex("favourites").insert({
+        users_id: req.session.user_id,
+        categories_id: req.body.favourite
+      }).then((favourites) => {
+        res.redirect(`/?categories=${req.body.favourite}`);
+      }).catch((error) => {
+        res.sendStatus(500);
+      });
+    }else{
       res.redirect(`/?categories=${req.body.favourite}`);
-    }).catch((error) => {
-      res.sendStatus(500);
-    });
+    }
   });
   return router;
 }
