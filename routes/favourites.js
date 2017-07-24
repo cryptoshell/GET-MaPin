541364930748d9fb6040b0e9260e8933ce9f965d
Kai19 <ngyankai@hotmail.com>
2017-07-24 05:14:55 +0000

    made css style for login and register

--- routes/favourites.js
+++ routes/favourites.js
@@ -6,20 +6,35 @@ function createRouter(knex) {
   const router  = express.Router();
 
   router.post("/", (req, res) => {
-    if(req.session.user_id){
-      knex("favourites").insert({
-        users_id: req.session.user_id,
-        categories_id: req.body.favourite
-      }).then((favourites) => {
-        res.redirect(`/?categories=${req.body.favourite}`);
-      }).catch((error) => {
-        res.sendStatus(500);
-      });
-    }else{
+    if (req.session.user_id) {
+      const faveCategory = knex("favourites")
+        .select(1)
+        .where({users_id: req.session.user_id, categories_id: req.body.favourite})
+
+        faveCategory.then((rows) => {
+          if (rows.length) {
+            return Promise.reject({
+              type: 409,
+              message: `${req.body.favourites} Favourite already exists`
+            });
+          } else {
+            knex("favourites").insert({
+              users_id: req.session.user_id,
+              categories_id: req.body.favourite
+            }).then((favourites) => {
+              res.redirect(`/?categories=${req.body.favourite}`);
+            }).catch((error) => {
+              res.sendStatus(500);
+            });
+          }
+        })
+    } else {
       res.redirect(`/?categories=${req.body.favourite}`);
     }
   });
   return router;
 }
+
+
 
 module.exports = createRouter;
