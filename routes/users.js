71707cb70d23bb3925f8cf21034cce013dfc58e2
Nikolai <nikolaigauer@gmail.com>
2017-07-24 03:36:02 +0000

    Add favourites feature with listed links of favourites from profile page

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,25 @@ function createRouter(knex) {
   const router  = express.Router();
 
   router.get("/users", (req, res) => {
-    knex('users').select('*').where('id', req.session.user_id).limit(1)
-    .then((result)=> {
-    let templateVars = {
-      user: req.session.user_id,
-      name: result[0].name};
-    res.render("users", templateVars);
-    }
-  )
+    const user = knex('users').select('*').where('id', req.session.user_id).limit(1);
+    const favouriteList = knex('favourites').select('*')
+      .where('users_id', req.session.user_id)
+      .join('categories', 'categories_id', '=', 'categories.id')
+
+    Promise.all([user, favouriteList])
+      .then((result)=> {
+       console.log(result[1])
+
+        let templateVars = {
+          user: req.session.user_id,
+          name: result[0][0].name,
+          favourites: result[1]
+        };
+        res.render("users", templateVars);
+      }).catch((error) => {
+        console.log(error)      
+      });
+    
 
   });
   return router;
